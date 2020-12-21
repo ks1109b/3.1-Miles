1be96b36cc5dfdbcf51c806b5d76692689f50b79
Kseniia Bloshkina <ksenia119@hotmail.com>
2020-12-22 00:55:16 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        long amount = 999666_00;
+        long bonus = amount / 20 / 100;
+        System.out.println(bonus);
+    }
+}
